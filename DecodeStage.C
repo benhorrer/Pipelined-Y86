6d40e04bce4e98dabd90178e33cc0d5a5ee921bb
benhorrer <benjaminhorrer@gmail.com>
2023-03-29 13:40:44 -0400

    partially implemented decode stage

--- DecodeStage.C
+++ DecodeStage.C
@@ -12,10 +12,27 @@
 #include "Status.h"
 #include "Debug.h"
 
-
 bool DecodeStage::doClockLow(PipeReg ** pregs, Stage ** stages)
 {
+    setEInput(pregs[2], pregs[1]->getstat(), pregs[1]->geticode(), pregs[1]->getifun(),
+                pregs[1]->getvalC(), pregs[1]->getvalP(),  )
+}
 
+void DecodeStage::setEInput(E * ereg, uint64_t stat, uint64_t icode, 
+                           uint64_t ifun, uint64_t valC, uint64_t valA,
+                           uint64_t valB, uint64_t dstE, uint64_t dstM,
+                           uint64_t srcA, uint64_t srcB)
+{
+   ereg->getstat()->setInput(stat);
+   ereg->geticode()->setInput(icode);
+   ereg->getifun()->setInput(ifun);
+   ereg->getvalC()->setInput(valC);
+   ereg->getvalA()->setInput(valA);
+   ereg->getvalB()->setInput(valB);
+   ereg->getdstE()->setInput(dstE);
+   ereg->getdstM()->setInput(dstM);
+   ereg->getsrcA()->setInput(srcA);
+   ereg->getsrcB()->setInput(srcB);
 }
 
 void DecodeStage::doClockHigh(PipeReg ** pregs)
