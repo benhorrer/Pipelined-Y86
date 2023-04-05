c021d688db423ed157d33a01ab07d6c17bc9ee19
Greyson Donato <gjd27@rocketmail.com>
2023-04-05 12:42:54 -0400

    worked on Execute Stage

--- ExecuteStage.C
+++ ExecuteStage.C
@@ -18,24 +18,41 @@ bool ExecuteStage::doClockLow(PipeReg ** pregs, Stage ** stages)
     E * ereg = (E *) pregs[EREG];
     M * mreg = (M *) pregs[MREG];
 
+    uint64_t Cnd = 0, valE = 0;
+
+    uint64_t eregIcode = ereg->geticode()->getOutput();
     
+    setMInput(mreg, ereg->getstat()->getOutput(), eregIcode,
+                ereg->getCnd()->getOutput(), ereg->getdstE()->getOutput(),
+                ereg->getdstM()->getOutput(), ereg->getvalA()->getOutput(),
+                Cnd, valE);
 
 }
 
 void ExecuteStage::setMInput(M * mreg, uint64_t stat, uint64_t icode,
-                            uint64_t Cnd, uint64_t valE, uint64_t valA, 
-                            uint64_t dstE, uint64_t dstM) {
-   mreg->getstat()->setInput(stat);
-   mreg->geticode()->setInput(icode);
-   mreg->getifun()->setInput(Cnd);
-   mreg->getvalC()->setInput(valE);
-   mreg->getvalA()->setInput(valA);
-   mreg->getdstE()->setInput(dstE);
-   mreg->getdstM()->setInput(dstM);
-    }
+                            uint64_t dstE, uint64_t dstM, uint64_t valA, 
+                            uint64_t Cnd, uint64_t valE) 
+{
+    mreg->getstat()->setInput(stat);
+    mreg->geticode()->setInput(icode);
+    mreg->getCnd()->setInput(Cnd);
+    mreg->getvalE()->setInput(valE);
+    mreg->getvalA()->setInput(valA);
+    mreg->getdstE()->setInput(dstE);
+    mreg->getdstM()->setInput(dstM);
+}
 
 void ExecuteStage::doClockHigh(PipeReg ** pregs)
 {
 
+    M * mreg = (E *) pregs[MREG];
+
+    mreg->getstat()->setInput(stat);
+    mreg->geticode()->setInput(icode);
+    mreg->getCnd()->setInput(Cnd);
+    mreg->getvalE()->setInput(valE);
+    mreg->getvalA()->setInput(valA);
+    mreg->getdstE()->setInput(dstE);
+    mreg->getdstM()->setInput(dstM);
 }
 
